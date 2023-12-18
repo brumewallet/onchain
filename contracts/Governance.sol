f9971d7e17b7d6131af285e7e7fde82277bd1765
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-12-18 22:18:24 +0100

    token

--- contracts/Governance.sol
+++ contracts/Governance.sol
@@ -6,12 +6,25 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
+contract Token is Ownable, ERC20, ERC20Burnable {
+
+    constructor(address initialOwner)
+        ERC20("Brume", "BRUME")
+        Ownable(initialOwner)
+    {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+    
+}
+
 contract Governance is Ownable, ERC20, ERC20Burnable {
 
     /*
      * The original token contract
      */
-    IERC20 public constant token = IERC20(0x0000000000000000000000000000000000000000);
+    IERC20 public immutable token = Token(address(this));
     
     /*
      * The timelock delay in seconds
