8f531d0b0e1b3cd319568aedb0f03432832afb60
Sean McCaffery <sean.mccaffery9@gmail.com>
2023-12-18 19:21:39 -0500

    test: add propose tests

--- test/Governance.t.sol
+++ test/Governance.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.20;
 
 import "@forge-std/Test.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "../contracts/Governance.sol";
 import "../contracts/Token.sol";
 
@@ -21,9 +22,9 @@ contract GovernanceTest is Test {
     address public constant VOTER_2 = 0x52a9EEf606115f488abd34194C73256F982A2Ab9;
     address public constant VOTER_3 = 0x5a65B0aA22f441Fd05eB0e81ff0dA6ecF07C8d8e;
 
-    // address[] public constant proposalTargets = [];
-    // uint256[] public constant values = [];
-    // bytes[] public constant calldatas = [];
+    address[] public proposalTargets1 = [VOTER_1];
+    uint256[] public values1 = [1234];
+    bytes[] public calldatas1 = new bytes[](2);
 
     /////////////////////////////
 
@@ -65,6 +66,22 @@ contract GovernanceTest is Test {
 
         assertEq(originalToken.balanceOf(VOTER_1), VOTER_1_INITIAL_AMOUNT);
         assertEq(governance.balanceOf(VOTER_1), 0);
+    }
+
+    // Propose by owner
+    function testProposeByOwner() public {
+        vm.prank(INITIAL_OWNER);
+        governance.propose(proposalTargets1, values1, calldatas1);
+        (uint256 callshash,) = governance.proposal();
+        
+        assertEq(governance.hashOf(proposalTargets1, values1, calldatas1), callshash);
+    }
+
+    // Propose by non-owner
+    function testProposeByNonOwner() public {
+        vm.prank(VOTER_1);
+        vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, VOTER_1));
+        governance.propose(proposalTargets1, values1, calldatas1);
     }
 
     /////////////////////////////
