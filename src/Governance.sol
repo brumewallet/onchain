3b455d7909e50469a7879fe2ab9e541deb6961f4
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2024-03-24 13:19:57 +0100

    freed

--- src/Governance.sol
+++ src/Governance.sol
@@ -153,15 +153,6 @@ contract Governance is Ownable, ERC20, ERC20Wrapper, ERC20Votes {
     }
 
     /**
-     * @dev Steal excess tokens from `account` if it has more than 25% of the total supply. This is used to prevent an address from owning more than 25% of the voting power and avoid legal issues.
-     */
-    function steal(address account) public {
-        if (balanceOf(account) > (totalSupply() / 4)) {
-            _transfer(account, _msgSender(), (totalSupply() / 4) - balanceOf(account));
-        }
-    }
-
-    /**
      * @dev Hash a set of transactions.
      */
     function hashOf(
