6f2f4f38662c636860f8f6fabc1b43fd867fce30
hazae41 <4405263+hazae41@users.noreply.github.com>
2024-09-09 13:03:17 +0700

    d

--- src/Art.sol
+++ src/Art.sol
@@ -14,7 +14,7 @@ contract Art is Ownable, ERC721, ERC721URIStorage, ERC721Enumerable, ERC721Royal
         Ownable(_msgSender())
     {}
 
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Royalty) returns (bool) {
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Royalty) returns (bool) {
         return super.supportsInterface(interfaceId);
     }
 
@@ -26,16 +26,32 @@ contract Art is Ownable, ERC721, ERC721URIStorage, ERC721Enumerable, ERC721Royal
         return super._update(to, tokenId, auth);
     }
 
-    function tokenURI(uint256 tokenId) public view virtual override(ERC721, ERC721URIStorage) returns (string memory) {
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
     }
 
-    function setDefaultRoyalty(uint96 _royalty) public onlyOwner {
-        _setDefaultRoyalty(owner(), _royalty);
+    function setDefaultRoyalty(uint96 feeNumerator) public onlyOwner {
+        _setDefaultRoyalty(owner(), feeNumerator);
+    }
+
+    function deleteDefaultRoyalty() public onlyOwner {
+        _deleteDefaultRoyalty();
+    }
+
+    function setTokenRoyalty(uint256 tokenId, address receiver, uint96 feeNumerator) public onlyOwner {
+        _setTokenRoyalty(tokenId, receiver, feeNumerator);
+    }
+
+    function resetTokenRoyalty(uint256 tokenId) public onlyOwner {
+        _resetTokenRoyalty(tokenId);
     }
 
     function setTokenURI(uint256 tokenId, string memory _tokenURI) public onlyOwner {
         _setTokenURI(tokenId, _tokenURI);
+    }
+
+    function mint(address to, uint256 tokenId) public onlyOwner {
+        _mint(to, tokenId);
     }
 
 }
